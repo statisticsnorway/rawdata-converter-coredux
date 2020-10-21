eb95b2bce8fa66bc984f95424fb150df08b05fc9
Kenneth Leine Schulstad <ks@udp.no>
2020-10-21 14:32:27 +0200

    Expect pseudo-secrets to be a Map

--- src/main/java/no/ssb/rawdata/converter/core/pseudo/FieldPseudonymizerFactory.java
+++ src/main/java/no/ssb/rawdata/converter/core/pseudo/FieldPseudonymizerFactory.java
@@ -7,14 +7,22 @@ import no.ssb.rawdata.converter.core.job.ConverterJobConfig;
 
 import javax.inject.Singleton;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Singleton
 public class FieldPseudonymizerFactory {
 
     private final List<PseudoSecret> pseudoSecrets;
 
-    public FieldPseudonymizerFactory(@Property(name="pseudo-secrets") List<PseudoSecret> pseudoSecrets) {
-        this.pseudoSecrets = pseudoSecrets;
+    public FieldPseudonymizerFactory(@Property(name="pseudo-secrets") Map<String, PseudoSecret> pseudoSecretMap) {
+        pseudoSecrets = pseudoSecretMap.entrySet().stream()
+          .map(e -> {
+              PseudoSecret secret = e.getValue();
+              secret.setId(e.getKey());
+              return secret;
+          })
+          .collect(Collectors.toList());
     }
 
     public FieldPseudonymizer newFieldPseudonymizer(ConverterJobConfig jobConfig) {
