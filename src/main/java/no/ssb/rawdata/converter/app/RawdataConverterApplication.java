8a5aa6857d29d076a6144a58be20fe56faea3d09
Kenneth Leine Schulstad <ks@udp.no>
2020-10-24 22:18:10 +0200

    Update RawdataConverterApplication logger

--- src/main/java/no/ssb/rawdata/converter/app/RawdataConverterApplication.java
+++ src/main/java/no/ssb/rawdata/converter/app/RawdataConverterApplication.java
@@ -1,21 +1,21 @@
 package no.ssb.rawdata.converter.app;
 
 import ch.qos.logback.classic.util.ContextInitializer;
-import lombok.extern.slf4j.Slf4j;
 import no.ssb.rawdata.converter.util.MavenArtifactUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Slf4j
 public abstract class RawdataConverterApplication {
+    protected RawdataConverterApplication() {}
+
     static {
         String logbackConfigurationFile = System.getenv("LOGBACK_CONFIGURATION_FILE");
         if (logbackConfigurationFile != null) {
             System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, logbackConfigurationFile);
         }
-    }
 
-    protected RawdataConverterApplication() {}
-
-    private static void logStartupInfo() {
+        Logger log = LoggerFactory.getLogger(RawdataConverterApplication.class);
         log.info("rawdata-converter-core version: {}", MavenArtifactUtil.findArtifactVersion("no.ssb.rawdata.converter", "rawdata-converter-coredux").orElse("unknown"));
     }
+
 }
